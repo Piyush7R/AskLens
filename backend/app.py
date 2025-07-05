41d3958cf08171ffba32ba4db8a0fdc901194601
Piyush Raj <142214168+Piyush7R@users.noreply.github.com>
2025-07-05 17:13:53 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -207,4 +207,5 @@ def not_found(e):
 
 # === 🚀 Run App ===
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
